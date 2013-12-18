55bc9e354ba235b74ab09e5f0e898449445b2be8
Mark Redar <mredar@gmail.com>
2013-12-17 17:04:16 -0800

    test a real harvest from command line

---
+++ test_harvest_script_call.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./start_harvest.bash mark.redar@ucop.edu 'Los Angeles Times Photographic Archive' UCLA 'UCLA Library Special Collections, Charles E. Young Research Library' OAI http://digital2.library.ucla.edu/oai2_0.do latimes
