90d5d7a8a016dfbfa747e5c773069f470b161de5
Mark Redar <mredar@gmail.com>
2014-06-26 15:34:34 -0700

    Fix broken test.

--- tests.py
+++ tests.py
@@ -1205,8 +1205,7 @@ class SolrUpdaterTestCase(TestCase):
         doc = pickle.load(f)
         sdoc = map_couch_to_solr_doc(doc)
         push_doc_to_solr(sdoc, mock_solr)
-        print('CALLS:{0}'.format( mock_solr.mock_calls))
-        mock_solr.add.assert_called_with({'repository': [u'Bancroft Library'], 'collection_name': u'Uchida (Yoshiko) photograph collection', 'subject': [u'Yoshiko Uchida photograph collection', u'Japanese American Relocation Digital Archive'], 'id': u'uchida-yoshiko-photograph-collection--http://ark.cdlib.org/ark:/13030/ft009nb05r', 'collection': {u'@id': u'https://registry.cdlib.org/api/v1/collection/23066', u'name': u'Uchida (Yoshiko) photograph collection'}, 'campus': [u'UC Berkeley']})
+        mock_solr.add.assert_called_with({'rights': [u'Transmission or reproduction of materials protected by copyright beyond that allowed by fair use requires the written permission of the copyright owners. Works not in the public domain cannot be commercially exploited without permission of the copyright owner. Responsibility for any use rests exclusively with the user.', u'The Bancroft Library--assigned', u'All requests to reproduce, publish, quote from, or otherwise use collection materials must be submitted in writing to the Head of Public Services, The Bancroft Library, University of California, Berkeley 94720-6000. See: http://bancroft.berkeley.edu/reference/permissions.html', u'University of California, Berkeley, Berkeley, CA 94720-6000, Phone: (510) 642-6481, Fax: (510) 642-7589, Email: bancref@library.berkeley.edu'], 'repository_name': [u'Bancroft Library'], 'url_item': u'http://ark.cdlib.org/ark:/13030/ft009nb05r', 'repository': [u'https://registry.cdlib.org/api/v1/repository/4/'], 'publisher': u'The Bancroft Library, University of California, Berkeley, Berkeley, CA 94720-6000, Phone: (510) 642-6481, Fax: (510) 642-7589, Email: bancref@library.berkeley.edu, URL: http://bancroft.berkeley.edu/', 'collection_name': u'Uchida (Yoshiko) photograph collection', 'format': u'mods', 'title': u'Neighbor', 'collection': u'https://registry.cdlib.org/api/v1/collection/23066', 'campus': [u'https://registry.cdlib.org/api/v1/campus/1/'], 'campus_name': [u'UC Berkeley'], 'relation': [u'http://www.oac.cdlib.org/findaid/ark:/13030/ft6k4007pc', u'http://bancroft.berkeley.edu/collections/jarda.html', u'hb158005k9', u'BANC PIC 1986.059--PIC', u'http://www.oac.cdlib.org/findaid/ark:/13030/ft6k4007pc', u'http://calisphere.universityofcalifornia.edu/', u'http://bancroft.berkeley.edu/'], 'type': u'image', 'id': u'uchida-yoshiko-photograph-collection--http://ark.cdlib.org/ark:/13030/ft009nb05r', 'subject': [u'Yoshiko Uchida photograph collection', u'Japanese American Relocation Digital Archive']})
 
     def test_map_couch_to_solr_doc(self):
         '''Test the mapping of a couch db source json doc to a solr schema
@@ -1214,8 +1213,6 @@ class SolrUpdaterTestCase(TestCase):
         '''
         f = open('pickled_couchdb_doc')
         doc = pickle.load(f)
-        print("SRC RES KEYS:{0}".format(doc['sourceResource'].keys()))
-        print("ORG REC KEYS:{0}".format(doc['originalRecord'].keys()))
         sdoc = map_couch_to_solr_doc(doc)
         self.assertEqual(sdoc['id'], doc['_id'])
         self.assertEqual(sdoc['id'], 'uchida-yoshiko-photograph-collection--http://ark.cdlib.org/ark:/13030/ft009nb05r')
