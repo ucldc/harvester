95e25c0e40c4d17f44217ba6bc4301a751115d63
Mark Redar <mredar@gmail.com>
2014-07-15 23:19:28 +0000

    Fix travis?

--- harvester/grab_solr_index.py
+++ harvester/grab_solr_index.py
@@ -3,7 +3,7 @@ import os
 import  ansible_run_pb
 
 def main():
-	code_dir = os.path.abspath(os.path.join(os.environ.get('HOME', '~'), 'code/harvester/harvester/'))
+	code_dir = os.path.abspath(os.path.join(os.environ.get('DIR_CODE', os.environ.get('HOME', '~')), 'code/harvester/harvester/'))
 	playbook = os.path.join(code_dir, 'grab-solr-index-playbook.yml')
 	inventory = os.path.join(code_dir, 'host_inventory')
 	print("======================PLAYBOOK:{0} INV:{1}".format(playbook, inventory))
