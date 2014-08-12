bc40749c5002dd713ad61672e918e4f90ef81c1d
Mark Redar <mredar@gmail.com>
2014-08-12 18:53:17 +0000

    Split out couchdb_pager, make sync to solr work better.

--- harvest_all_images.py
+++ harvest_all_images.py
@@ -9,47 +9,12 @@ import datetime
 import time
 from md5s3stash import md5s3stash
 import couchdb
+from harvester.couchdb_pager import coucdb_pager
 
 BUCKET_BASE = 'ucldc'
 SERVER_COUCHDB = 'https://54.84.142.143/couchdb'
 DB_COUCHDB = 'ucldc'
 COUCH_VIEW = 'all_provider_docs/by_provider_name'
-
-def couchdb_pager(db, view_name='_all_docs',
-                  startkey=None, startkey_docid=None,
-                  endkey=None, endkey_docid=None, bulk=5000, **extra_options):
-    # Request one extra row to resume the listing there later.
-    options = {'limit': bulk + 1}
-    print("EXTRA: {}".format(extra_options))
-    if extra_options:
-         options.update(extra_options)
-    print("OPTS:{}".format(options))
-    if startkey:
-        options['startkey'] = startkey
-        if startkey_docid:
-            options['startkey_docid'] = startkey_docid
-    if endkey:
-        options['endkey'] = endkey
-        if endkey_docid:
-            options['endkey_docid'] = endkey_docid
-    done = False
-    while not done:
-        view = db.view(view_name, **options)
-        rows = []
-        # If we got a short result (< limit + 1), we know we are done.
-        if len(view) <= bulk:
-            done = True
-            rows = view.rows
-        else:
-            # Otherwise, continue at the new start position.
-            rows = view.rows[:-1]
-            last = view.rows[-1]
-            options['startkey'] = last.key
-            options['startkey_docid'] = last.id
-
-        for row in rows:
-            yield row
-
 
 def get_isShownBy(doc):
     best_image = None
--- harvester/sync_couch_to_solr.py
+++ harvester/sync_couch_to_solr.py
@@ -1,24 +1,34 @@
+'''Push contents of couchdb to solr index'''
 import os, sys
 import urllib
 import requests
 import solr
+import couchdb
 from solr import SolrException
 
 from solr_updater import map_couch_to_solr_doc, push_doc_to_solr
 from solr_updater import URL_SOLR
 from solr_updater import URL_COUCHDB, COUCHDB_DB
 
+from harvester.couchdb_pager import couchdb_pager
+
 def main(url_solr=URL_SOLR, url_couchdb=URL_COUCHDB, couchdb_db=COUCHDB_DB):
     solr_db = solr.Solr(url_solr)
-    resp=requests.get('/'.join((url_couchdb, couchdb_db, '_all_docs')))
-    j=resp.json()
-    rows=j['rows']
-    print "DOCS TO GRAB", len(rows)
+    s = couchdb.Server(url=url_couchdb)
+    db = s[couchdb_db]
+    v = couchdb_pager(db, include_docs='true')
     #update or create new solr doc for each couchdb doc
-    for row in rows:
-        if not '_design' in row['id']:
-            doc = requests.get('/'.join((url_couchdb, couchdb_db, urllib.quote_plus(row['id']))))
-            doc_couch = doc.json()
+    for r in v:
+        doc_couch = r.doc
+        if not '_design' in doc_couch['_id']:
+            if not isinstance(doc_couch['originalRecord']['collection'], list):
+                doc_couch['originalRecord']['collection'] = [
+                                doc_couch['originalRecord']['collection'],]
+            if not isinstance(doc_couch['sourceResource']['collection'], list):
+                doc_couch['sourceResource']['collection'] = [
+                                doc_couch['sourceResource']['collection'],]
+            print("srcRes.COLLECTION: {}".format(doc_couch['sourceResource']['collection']))
+            db.save(doc_couch)
             doc_solr = push_doc_to_solr(map_couch_to_solr_doc(doc_couch),
                                         solr_db=solr_db)
     solr_db.commit()
