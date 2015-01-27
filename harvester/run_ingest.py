331c7a349e2d507ca52b24619ffc9cb1c4570e0b
Mark Redar <mredar@gmail.com>
2015-01-27 19:27:53 +0000

    Change default to *NOT* run image_harvest

--- harvester/run_ingest.py
+++ harvester/run_ingest.py
@@ -58,7 +58,7 @@ def main(user_email, url_api_collection, log_handler=None,
          mail_handler=None, dir_profile='profiles', profile_path=None,
          config_file='akara.ini', redis_host=None, redis_port=None,
          redis_pswd=None, redis_timeout=600,
-         run_image_harvest=True):
+         run_image_harvest=False):
     '''Runs a UCLDC ingest process for the given collection'''
     emails = [user_email]
     if EMAIL_SYS_ADMIN:
