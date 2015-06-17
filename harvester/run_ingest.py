92248f18b15f28de32a88b77efe69c0105c89f8a
Mark Redar <mredar@gmail.com>
2015-06-17 12:17:49 -0700

    fix var name

--- harvester/run_ingest.py
+++ harvester/run_ingest.py
@@ -136,7 +136,7 @@ def main(user_email, url_api_collection, log_handler=None,
     if run_image_harvest:
         job = queue_image_harvest(config['redis_host'], config['redis_port'],
                                   config['redis_password'], config['redis_connect_timeout'],
-                                  config_harvest['couchdb_url'],
+                                  config['couchdb_url'],
                                   collection.id,
                                   rq_queue,
                                   object_auth=collection.auth)
