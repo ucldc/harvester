00984ff91f8cbd2c24d02d1328c24b315c7e1606
Mark Redar <mredar@gmail.com>
2015-06-29 15:27:25 -0700

    message fix for solr_up. add --yes param to delete

--- harvester/solr_updater.py
+++ harvester/solr_updater.py
@@ -71,13 +71,15 @@ COUCHDOC_ORIGINAL_RECORD_TO_SOLR_MAPPING = {
 def has_required_fields(doc):
     '''Check the couchdb doc has required fields'''
     if 'sourceResource' not in doc:
-        raise KeyError('+++++OMITTED: Doc:{0} has no sourceResource.'.format(doc['id']))
+        raise KeyError(
+            '+++++OMITTED: Doc:{0} has no sourceResource.'.format(doc['_id']))
     if 'title' not in doc['sourceResource']:
-        raise KeyError('+++++OMITTED: Doc:{0} has no title.'.format(doc['id']))
+        raise KeyError('+++++OMITTED: Doc:{0} has no title.'.format(doc['_id']))
     if 'image' == doc['sourceResource'].get('type', '').lower():
         #if doesnt have a reference_image_md5, reject
         if 'object' not in doc:
-            raise KeyError('+++++OMITTED: Doc:{0} is image type with no harvested image.'.format(doc['id']))
+            raise KeyError(
+                '+++++OMITTED: Doc:{0} is image type with no harvested image.'.format(doc['_id']))
     return True
 
 def add_slash(url):
