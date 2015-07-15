5bc83496ef7a38ea30fc1b29adda19cb32b49cba
Mark Redar <mredar@gmail.com>
2015-07-14 17:19:39 -0700

    Add couchdb counts and OAC counts where appropriate.

--- scripts/report_harvested_to_solr.py
+++ scripts/report_harvested_to_solr.py
@@ -6,15 +6,19 @@ import csv
 import codecs
 import cStringIO
 import datetime
+from harvester.couchdb_init import get_couchdb
+from harvester.fetcher import OAC_XML_Fetcher
 
 py_version = sys.version_info
 if py_version.major == 2 and py_version.minor == 7 and py_version.micro > 8:
     #disable ssl verification
     import ssl
     ssl._create_default_https_context = ssl._create_unverified_context
-   
+
 SOLR_URL = os.getenv('UCLDC_SOLR_URL',
         'https://ucldc-solr-stage.elasticbeanstalk.com/solr/')
+COUCHDB_URL = os.getenv('COUCHDB_URL',
+        'https://51.10.100.133/couchdb')
 
 'https://ucldc-solr-stage.elasticbeanstalk.com/solr/query?q=*:*&rows=0&facet=true&facet.field=collection_url&facet.limit=1000'
 
@@ -48,7 +52,6 @@ class UnicodeWriter:
         for row in rows:
             self.writerow(row)
 
-
 def get_indexed_collection_list(SOLR):
     '''Use the facet query to extract the collection urls for 
     collections in index.
@@ -63,14 +66,22 @@ def get_indexed_collection_list(SOLR):
     collection_urls = query_results.facet_counts['facet_fields']['collection_url']
     return [(c_url, num) for c_url, num in collection_urls.items()]
 
+def get_couch_count(cdb, cid):
+    view_name='_design/all_provider_docs/_view/by_provider_name_count'
+    results = cdb.view(view_name,
+                    key=c.id)
+    for row in results:
+        return row.value
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser(
         description='Make csv report of indexed collections')
     parser.add_argument('auth_token', help='Authentication token')
     parser.add_argument('--solr_url', help='Solr index url')
+    parser.add_argument('--couchdb_url', help='CouchDB url')
     args = parser.parse_args()
-    solr_url = args.sorl_url if args.solr_url else SOLR_URL
-    print "AUTH:{}".format(args.auth_token)
+    solr_url = args.solr_url if args.solr_url else SOLR_URL
+    print "SOLR_URL:{}".format(solr_url)
     SOLR = solr.SearchHandler(
                 solr.Solr(
                     solr_url,
@@ -80,17 +91,41 @@ if __name__=="__main__":
                 ),
             "/query"
     )
+    if args.couchdb_url:
+        cdb = get_couchdb(url_couchdb=couchdb_url, dbname='ucldc')
+    else:
+        cdb = get_couchdb(dbname='ucldc')
     collections = get_indexed_collection_list(SOLR)
     date_to_minute = datetime.datetime.now().strftime('%Y%m%d-%H%M')
     fname = 'indexed_collections-{}.csv'.format(date_to_minute)
     with open(fname, 'wb') as csvfile:
         csvwriter = UnicodeWriter(csvfile)
         csvwriter.writerow(('Collection Name', 'Collection URL',
-                'Number in index', 'Repository Name', 'Repository URL',
+                'Number in index', 'Number in couchdb', 'Number in OAC',
+                'Couch missing in solr', 'OAC missing in couch',
+                'Repository Name', 'Repository URL',
                 'Campus'))
         for c_url, num in collections:
-            c = Collection(c_url)
-            csvwriter.writerow((c['name'], c_url, str(num),
+            try:
+                c = Collection(c_url)
+            except ValueError, e:
+                print "NO COLLECTION FOR :{}".format(c_url)
+                continue
+            couch_count = get_couch_count(cdb, c.id)
+            solr_equal_couch = False
+            if couch_count == num:
+                solr_equal_couch = True
+            oac_num = None
+            couch_equal_oac = None
+            if c.harvest_type == 'OAC':
+                fetcher = OAC_XML_Fetcher(c.url_harvest, c.harvest_extra_data)
+                oac_num = fetcher.totalDocs
+                if couch_count == oac_num:
+                    couch_equal_oac = True
+                else:
+                    couch_equal_oac = False
+            csvwriter.writerow((c['name'], c_url, str(num), str(couch_count),
+                str(oac_num), str(solr_equal_couch), str(couch_equal_oac),
                 c.repository[0]['name'] if len(c.repository) else '',
                 c.repository[0]['resource_uri']if len(c.repository) else '',
                 c.campus[0]['name'] if len(c.campus) else ''))
