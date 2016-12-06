1e66c2555ba0026acebf00ca4f6e8e4fcf20f90a
Mark Redar <mredar@gmail.com>
2016-12-06 14:09:22 -0800

    Add SNS notifications to end of harvesting steps.

--- harvester/couchdb_sync_db_by_collection.py
+++ harvester/couchdb_sync_db_by_collection.py
@@ -10,13 +10,14 @@ from os import environ
 import sys
 from harvester.collection_registry_client import Collection
 from harvester.couchdb_init import get_couchdb
-from harvester.couchdb_pager import couchdb_pager
 from harvester.post_processing.couchdb_runner import CouchDBCollectionFilter
 from harvester.post_processing.couchdb_runner import get_collection_doc_ids
+from harvester.sns_message import publish_to_harvesting
 
 COUCHDB_VIEW_COLL_IDS = 'all_provider_docs/by_provider_name'
 
 # use couchdb_init to get environment couchdb
+
 
 def delete_id_list(ids, _couchdb=None):
     '''For a list of couchdb ids & given couchdb, delete the docs'''
@@ -29,33 +30,35 @@ def delete_id_list(ids, _couchdb=None):
         _couchdb.delete(doc)
         deleted.append(did)
         print >> sys.stderr, "DELETED: {0}".format(did)
-        num_deleted +=1
-    return num_deleted, deleted 
+        num_deleted += 1
+    return num_deleted, deleted
+
 
 def delete_collection(cid):
     print >> sys.stderr, "DELETING COLLECTION: {}".format(cid)
     _couchdb = get_couchdb()
-    rows = CouchDBCollectionFilter(collection_key=cid,
-                                        couchdb_obj=_couchdb
-                                        )
+    rows = CouchDBCollectionFilter(collection_key=cid, couchdb_obj=_couchdb)
     ids = [row['id'] for row in rows]
-    return delete_id_list(ids, _couchdb=_couchdb)
+    num_deleted, deleted_docs = delete_id_list(ids, _couchdb=_couchdb)
+    publish_to_harvesting(
+        'Deleted CouchDB Collection {}'.format(cid),
+        'Deleted {} documents from CouchDB collection {}'.format(num_deleted,
+                                                                 cid))
+    return num_deleted, deleted_docs
+
 
 def collection_ready_for_publication(url_api_collection):
-    '''Check if the collection has "ready_for_publication" currently 
+    '''Check if the collection has "ready_for_publication" currently
     checked
     '''
-    try:
-        collection = Collection(url_api_collection)
-    except Exception, e:
-        msg = 'Exception in Collection {}, init {}'.format(url_api_collection,
-                                                           str(e))
-        logbook.error(msg)
-        raise e
+    collection = Collection(url_api_collection)
     return collection.ready_for_publication
 
-def update_from_remote(doc_id, url_remote_couchdb=None, couchdb_remote=None,
-        couchdb_env=None):
+
+def update_from_remote(doc_id,
+                       url_remote_couchdb=None,
+                       couchdb_remote=None,
+                       couchdb_env=None):
     '''Update the environment's couchdb from a remote couchdb document
     '''
     if not couchdb_remote:
@@ -65,8 +68,8 @@ def update_from_remote(doc_id, url_remote_couchdb=None, couchdb_remote=None,
     doc = couchdb_remote.get(doc_id)
     # need to remove the revision data, as will be different
     del doc['_rev']
-    #if doc exists, need to update metadata for the existing document
-    #and then save that, due to revision number in couch
+    # if doc exists, need to update metadata for the existing document
+    # and then save that, due to revision number in couch
     doc_in_target = couchdb_env.get(doc_id)
     if doc_in_target:
         doc_in_target.update(doc)
@@ -77,7 +80,10 @@ def update_from_remote(doc_id, url_remote_couchdb=None, couchdb_remote=None,
         couchdb_env[doc_id] = doc_no_rev
         print >> sys.stderr, "created {}".format(doc_id)
 
-def queue_update_from_remote(queue, url_api_collection, url_couchdb_source=None):
+
+def queue_update_from_remote(queue,
+                             url_api_collection,
+                             url_couchdb_source=None):
     '''for this environment, put a couchdb doc id on another environments
     queue. This environment's couchdb url becomes the remote in the call to
     update_from_remote
@@ -85,40 +91,44 @@ def queue_update_from_remote(queue, url_api_collection, url_couchdb_source=None)
     '''
     pass
 
-def update_collection_from_remote(url_remote_couchdb, url_api_collection, 
-        delete_first=True):
+
+def update_collection_from_remote(url_remote_couchdb,
+                                  url_api_collection,
+                                  delete_first=True):
     '''Update a collection from a remote couchdb.
     '''
     if delete_first:
         delete_collection(url_api_collection.rsplit('/', 2)[1])
     collection = Collection(url_api_collection)
-    #guard against updating production for not ready_for_publication
-    #collections
+    # guard against updating production for not ready_for_publication
+    # collections
     if 'prod' in environ.get('DATA_BRANCH', ''):
         if not collection.ready_for_publication:
             raise Exception(
-'In PRODUCTION ENV and collection {} not ready for publication'.format(collection.id))
+                'In PRODUCTION ENV and collection {} not ready for '
+                'publication'.format(collection.id))
     doc_ids = get_collection_doc_ids(collection.id, url_remote_couchdb)
     couchdb_remote = get_couchdb(url_remote_couchdb)
     couchdb_env = get_couchdb()
     for doc_id in doc_ids:
-        update_from_remote(doc_id,
-                couchdb_remote=couchdb_remote,
-                couchdb_env=couchdb_env)
+        update_from_remote(
+            doc_id, couchdb_remote=couchdb_remote, couchdb_env=couchdb_env)
+
 
 def main(url_remote_couchdb, url_api_collection):
     '''Update to the current environment's couchdb a remote couchdb collection
     '''
     update_collection_from_remote(url_remote_couchdb, url_api_collection)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(
         description='Update current env couchdb from a remote couchdb for \
                 given collection')
-    parser.add_argument('url_remote_couchdb',
-                        help='URL to the remote (source) couchdb')
-    parser.add_argument('url_api_collection',
-                        help='Registry api endpoint for the collection')
+    parser.add_argument(
+        'url_remote_couchdb', help='URL to the remote (source) couchdb')
+    parser.add_argument(
+        'url_api_collection', help='Registry api endpoint for the collection')
     args = parser.parse_args(sys.argv[1:])
     main(args.url_remote_couchdb, args.url_api_collection)
