fe65e85e0a29341a6eebbb1bafb28b8d1225abfc
Mark Redar <mredar@gmail.com>
2017-06-13 14:59:27 -0700

    Add RQ exception handler to report to SNS topic

--- harvester/rq_worker_sns_msgs.py
+++ harvester/rq_worker_sns_msgs.py
@@ -7,6 +7,16 @@ from harvester.sns_message import publish_to_harvesting
 logger = logging.getLogger(__name__)
 
 
+def exception_to_sns(job, *exc_info):
+    '''Make an exception handler to report exceptions to SNS msg queue'''
+    subject = 'FAILED: job {}'.format(job.description)
+    message = 'ERROR: job {} failed\n{}'.format(
+        job.description,
+        exc_info[1])
+    logging.error(message)
+    publish_to_harvesting(subject, message)
+
+
 class SNSWorker(Worker):
     def execute_job(self, job, queue):
         """Spawns a work horse to perform the actual work and passes it a job.
@@ -18,12 +28,12 @@ class SNSWorker(Worker):
         subject = 'Worker {} starting job {}'.format(
             worker_name,
             job.description)
-        publish_to_harvesting(subject, subject)
+        #publish_to_harvesting(subject, subject)
         self.set_state('busy')
         self.fork_work_horse(job, queue)
         self.monitor_work_horse(job)
         subject = 'Worker {} finished job {}'.format(
             worker_name,
             job.description)
-        publish_to_harvesting(subject, subject)
+        #publish_to_harvesting(subject, subject)
         self.set_state('idle')
