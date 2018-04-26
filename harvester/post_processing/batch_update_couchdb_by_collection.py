6b424dea6c64aef51900911fe7556afbf9a3a7af
Matthew McKinley <mmckinle@ucop.edu>
2018-04-26 16:46:59 -0700

    batch update CouchDB script -- v1 final

--- harvester/post_processing/batch_update_couchdb_by_collection.py
+++ harvester/post_processing/batch_update_couchdb_by_collection.py
@@ -1,56 +1,43 @@
 #! /bin/env python
-from os import environ
 import sys
-from copy import deepcopy
-from harvester.collection_registry_client import Collection
 from harvester.couchdb_init import get_couchdb
 from harvester.post_processing.couchdb_runner import CouchDBCollectionFilter
-from harvester.post_processing.couchdb_runner import get_collection_doc_ids
 from harvester.sns_message import publish_to_harvesting
 from harvester.sns_message import format_results_subject
-from akara import logger
 
 PATH_DELIM = '/'
 
-def setprop(obj,path,val,keyErrorAsNone=False):
+
+def setprop(obj, path, val, keyErrorAsNone=False):
     """
     Sets the value of the key identified by interpreting
     the path as a delimited hierarchy of keys. Enumerate
     if object is list.
     """
     if '/' not in path:
-        logger.error(type(obj))
-        logger.error(obj)
         if type(obj) == list:
-            logger.error("HERE")
-            logger.error(obj)
             for t in (t for t in obj if t[path]):
-                logger.error(t)
-                logger.error(path)
                 t[path] = val
                 return
         else:
             if path not in obj:
                 if not keyErrorAsNone:
-                    raise KeyError('Path not found in object: %s'%(path))
+                    raise KeyError('Path not found in object: %s' % (path))
                 else:
                     return None
             else:
-                logger.error("THERE")
-                logger.error(obj)
-                logger.error(path)
                 obj[path] = val
                 return
 
-    pp,pn = tuple(path.lstrip(PATH_DELIM).split(PATH_DELIM,1))
-    logger.error(pp)
+    pp, pn = tuple(path.lstrip(PATH_DELIM).split(PATH_DELIM, 1))
     if pp not in obj:
         if not keyErrorAsNone:
-            raise KeyError('Path not found in object: %s (%s)'%(path,pp))
+            raise KeyError('Path not found in object: %s (%s)' % (path, pp))
         else:
             return None
 
-    return setprop(obj[pp],pn,val,keyErrorAsNone)
+    return setprop(obj[pp], pn, val, keyErrorAsNone)
+
 
 def update_by_id_list(ids, fieldName, newValue, _couchdb=None):
     '''For a list of couchdb ids, given field name and new value, update the doc[fieldname] with "new value"'''
@@ -67,17 +54,17 @@ def update_by_id_list(ids, fieldName, newValue, _couchdb=None):
         num_updated += 1
     return num_updated, updated
 
+
 def update_couch_docs_by_collection(cid, fieldName, newValue):
     print >> sys.stderr, "UPDATING DOCS FOR COLLECTION: {}".format(cid)
     _couchdb = get_couchdb()
     rows = CouchDBCollectionFilter(collection_key=cid, couchdb_obj=_couchdb)
     ids = [row['id'] for row in rows]
-    num_updated, updated_docs = update_by_id_list(ids, fieldName, newValue, _couchdb=_couchdb)
+    num_updated, updated_docs = update_by_id_list(
+        ids, fieldName, newValue, _couchdb=_couchdb)
     subject = format_results_subject(cid,
                                      'Updated documents from CouchDB {env} ')
     publish_to_harvesting(
-        subject,
-        'Updated {} documents from CouchDB collection CID: {}'.format(
-            num_updated,
-            cid))
+        subject, 'Updated {} documents from CouchDB collection CID: {}'.format(
+            num_updated, cid))
     return num_updated, updated_docs
